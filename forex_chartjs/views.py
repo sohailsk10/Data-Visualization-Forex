0a604c63b14627240b2c834925fe63e552a6c849
neomoment-github <neomoment.ai@gmail.com>
2022-11-16 17:01:07 +0530

    CSS

--- forex_chartjs/views.py
+++ forex_chartjs/views.py
@@ -91,19 +91,24 @@ def get_currency(request):
     mydb = psycopg2.connect(
         database="postgres", user='postgres', password='admin', host='127.0.0.1', port='5432')
 
-    currency__ = ""
-    interval__ = ""
-
-    if request.GET.get('currency'):
-        currency__ = request.GET.get('currency')
-
-    if request.GET.get('interval'):
-        interval__ = request.GET.get('interval')
-        interval__ = interval__ + "Min"
+    mydb.autocommit = True
+    cursor = mydb.cursor()
 
     currency = "AUDUSD"
-    time_interval = "15Min"
-    mydb.autocommit = True
+    currency__ = ""
+    if request.GET.get('currency'):
+        currency__ = request.GET.get('currency')
+        print(currency__)
+    print(request.GET)
+    print("CURRENCY--------------------------", currency__)
+
+    if currency__ == '':
+        sql_current_price = "SELECT current_price from currency_buy_sell where currency = '" + currency + "' "
+    else:
+        sql_current_price = "SELECT current_price from currency_buy_sell where currency = '" + currency__ + "' "
+    cursor.execute(sql_current_price)
+    result_current_price = cursor.fetchall()[0][0]
+    cursor = None
     cursor = mydb.cursor()
 
     if currency__ == '':
@@ -112,15 +117,6 @@ def get_currency(request):
         sql_query_buy = "SELECT buy from currency_buy_sell where currency = '" + currency__ + "' "
     cursor.execute(sql_query_buy)
     result_buy = cursor.fetchall()[0][0]
-    # print("result_buy", result_buy)
-
-    if currency__ == '':
-        sql_current_price = "SELECT current_price from currency_buy_sell where currency = '" + currency + "' "
-    else:
-        sql_current_price = "SELECT current_price from currency_buy_sell where currency = '" + currency__ + "' "
-    cursor.execute(sql_current_price)
-    result_current_price = cursor.fetchall()[0][0]
-    # print(result_current_price)
 
     if currency__ == '':
         sql_query_sell = "SELECT sell from currency_buy_sell where currency = '" + currency + "' "
@@ -128,41 +124,12 @@ def get_currency(request):
         sql_query_sell = "SELECT sell from currency_buy_sell where currency = '" + currency__ + "' "
     cursor.execute(sql_query_sell)
     result_sell = cursor.fetchall()[0][0]
-    # print("result_sell", result_sell)
-
-    if currency__ == '':
-        sql_query_predicted_high_low = "select * from predicted_high_low"
-    else:
-        sql_query_predicted_high_low = "select * from predicted_high_low where currency = '" + currency__ + "'"
-
-    cursor.execute(sql_query_predicted_high_low)
-    result_high_low = cursor.fetchall()
-
-    if currency__ != '' and interval__ == '':
-        sql_query_historical_data = "Select * from historical_data where currency = '" + currency__ + "'"
-    elif currency__ != '' and interval__ != '':
-        sql_query_historical_data = "Select * from historical_data where currency = '" + currency__ + "' and time_interval = '" + interval__ + "'"
-    else:
-        sql_query_historical_data = "Select * from historical_data"
-
-    # print("sql_query_historical_data", sql_query_historical_data)
-    cursor.execute(sql_query_historical_data)
-    result_historical = cursor.fetchall()
-
-    currency_ = Currency.objects.all()
-    time_interval = Interval.objects.all()
 
     response = {
-        "Buy": result_buy,
-        "Sell": result_sell,
-        "high_low": result_high_low,
-        "historical_data": result_historical,
-        "Get_currency": currency_,
-        "Get_interval": time_interval,
-        "currency": currency__,
-        "current_price": result_current_price,
+        'current_price': result_current_price,
+        'result_buy': str(result_buy),
+        'result_sell': str(result_sell),
     }
-
     return JsonResponse(response)
 
 def get_current_price(request):
@@ -176,7 +143,7 @@ def get_current_price(request):
     currency__ = ""
     if request.GET.get('currency'):
         currency__ = request.GET.get('currency')
-    print("CURRENCY--------------------------", currency)
+    print("CURRENCY--------------------------", currency__)
 
     if currency__ == '':
         sql_current_price = "SELECT current_price from currency_buy_sell where currency = '" + currency__ + "' "
@@ -184,7 +151,6 @@ def get_current_price(request):
         sql_current_price = "SELECT current_price from currency_buy_sell where currency = '" + currency + "' "
     cursor.execute(sql_current_price)
     result_current_price = cursor.fetchall()[0][0]
-    # currency_params = request.GET.get('currency', None)
 
     response = {
         'current_price': result_current_price
